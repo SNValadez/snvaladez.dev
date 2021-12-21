db3a78a39d33adc2d1fbcb8ceb5270e551bd84e9
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-21 12:50:30 -0800

    create header and nav

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css"
+import Nav from "../Nav"
+
+function Header() {
+
+    return (
+        <header>
+            <h1>
+                S. N. Valadez
+            </h1>
+            <Nav >
+            
+           </Nav>
+        </header>
+    )
+
+}
+
+export default Header;
