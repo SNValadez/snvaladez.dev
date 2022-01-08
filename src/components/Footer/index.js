9f4ccd32814d87efe8f31e61bfa58ce0750f23a2
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-07 23:39:55 -0800

    create Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./style.css"
+
+function Footer() {
+    return (
+        <div className="footer-con">
+            <div className="footer-par">
+                <img src={require ("../../../assets/Header-Home/swirly-bg.png")} alt="error message"/>
+            </div>
+            
+        </div>
+    )
+}
+
+
+export default Footer;
