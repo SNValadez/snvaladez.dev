c099930a58e91ee4e2fb3c0e49fcde5de395aa08
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-08 15:31:19 -0800

    AnimeWishes component update

---
+++ src/utilities/AnimeWishes.js
@@ -0,0 +1,22 @@
+import ScreamService from "./ScreamService";
+
+export default class AnimeWishes {
+
+
+static animations = new Animations();
+
+fadeInScream =(scream_name)=>{
+    let scream = document.getElementById(scream_name);
+
+    if(!scream_name || !scream)
+
+    return
+
+
+    scream.style.opacity="6";
+    scream.style.transform="translateY(2px)";
+}
+
+
+
+}
