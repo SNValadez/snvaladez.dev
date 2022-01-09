947fe9ec165b586c42c7ad84d12c830b5cec72aa
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-08 18:20:37 -0800

    temporary BaseBoost suspension

--- src/components/Main/BaseBoost/index.js
+++ src/components/Main/BaseBoost/index.js
@@ -1,122 +1,127 @@
-import React, { useState } from 'react'
-import {TOTAL_SCREAM, GET_SCREAM_INDIE} from "../../../utilities/comUtils"
+// import React, { useState } from 'react'
+// import {TOTAL_SCREAM, GET_SCREAM_INDIE} from "../../../utilities/comUtils"
 
-import ScreamService from "../../../utilities/ScreamService"
-import "./style.css"
+// import ScreamService from "../../../utilities/ScreamService"
+// import "./style.css"
 
-import {faBars, faThermometerQuarter} from "@fortawesome/free-solid-svg-icons"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+// import {faBars, faThermometerQuarter} from "@fortawesome/free-solid-svg-icons"
+// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
-function BaseBoost() {
+// function BaseBoost() {
 
-    const [showBaseOptions, setShowBaseOptions] = useState(false);
-    const [selectedScream, setSelectedScream] = useState(0);
+//     const [showBaseOptions, setShowBaseOptions] = useState(false);
+//     const [selectedScream, setSelectedScream] = useState(0);
 
-const updateCurrentScream = (currentScream)=>{
+// const updateCurrentScream = (currentScream)=>{
 
-if(!currentScream || !currentScream.screamInView)
-return;
-let screamIndex = GET_SCREAM_INDIE(currentScream.screamInView)
-if(screamIndex < 0)
-return
-
-}
+// if(!currentScream || !currentScream.screamInView)
+// return;
+// let screamIndex = GET_SCREAM_INDIE(currentScream.screamInView)
 
 
+// if(screamIndex < 0)
+// return
 
-let currentScreamSub = ScreamService.currentScreamBroadCaster.subscribe(updateCurrentScream)
+// }
 
 
 
-const getBaseBoostOptions = () => {
-    return(
-        TOTAL_SCREAM.map((scream, i)=> (
-
-            <div key={scream.scream_name} className={getBaseBoostOptionsClass(i)}
-            onClick={() => switchScream(i, scream)}>
+// let currentScreamSub = ScreamService.currentScreamBroadCaster.subscribe(updateCurrentScream)
 
 
 
+// const getBaseBoostOptions = () => {
+//     return(
+//         TOTAL_SCREAM.map((scream, i)=> (
+
+//             <div key={scream.scream_name} className={getBaseBoostOptionsClass(i)}
+//             onClick={() => switchScream(i, scream)}>
 
 
 
 
-                <span>{scream.scream_name}</span>
-            </div>
-
-        )
-
-        )
-    )
-}
-
-
-        const getBaseBoostOptionsClass =(index) =>{
-
-    let classy = "baseboost-option";
-
-    if (index < TOTAL_SCREAM.length -1)
-    classy += "baseboost-option-sep";
-
-    if (selectedScream === index)
-    classy += "selected-baseboost-op";
-
-
-    return 
-
-}
 
 
 
-        const switchScream=(index, scream)=>{
+//                 <span>{scream.scream_name}</span>
+//             </div>
+
+//         )
+
+//         )
+//     )
+// }
 
 
-            let screamComponent = document.getElementById(scream.scream_name);
-                if (!screamComponent) return;
+//         const getBaseBoostOptionsClass =(index) =>{
+
+//     let classy = "baseboost-option";
+
+//     if (index < TOTAL_SCREAM.length -1)
+//     //
+//     classy += "baseboost-option-sep";
+
+//     if (selectedScream === index)
+//     classy += "selected-baseboost-op";
 
 
-                screamComponent.scrollIntoView({ behavior: "smooth" });
+//     return 
 
-                setShowBaseOptions(false);
-
-                            setSelectedScream(index)
-
-
-
-        };
-
-
-
-    return (
-        <div>
-
-           <div className="baseboost-option" onClick={() => setShowBaseOptions(!showBaseOptions)}>
+// }
 
 
 
-                        <div className="baseboost-parent">
+//         const switchScream=(index, scream)=>{
 
 
-                            <div className="baseboost-hamburger" onClick={()=> setShowBaseOptions(!showBaseOptions)}>
-                                <FontAwesomeIcon className="baseboost-hamburger-bars" icon={faBars} />
-
-                            </div>
+//             let screamComponent = document.getElementById(scream.scream_name);
+//                 if (!screamComponent) return;
 
 
-                            <div className="baseboost-logo">
-                                <span>*S. N. Valadez</span>
-                            </div>
+//                 screamComponent.scrollIntoView({ behavior: "smooth" });
+
+//                 setShowBaseOptions(false);
+
+//                             setSelectedScream(index)
 
 
-                            <div className={showBaseOptions ? "baseboost-options show-hamburger-options" : "baseboost-options"}></div>
 
-                        </div>
-
-           </div>
-
-        </div>
-    )
-    }
+//         };
 
 
-export default BaseBoost;
+
+//     return (
+//         <div>
+
+// //            <div className="baseboost-option" onClick={() => setShowBaseOptions(!showBaseOptions)}>
+
+
+
+// //                         <div className="baseboost-parent">
+
+
+// //                             <div className="baseboost-hamburger" onClick={()=> setShowBaseOptions(!showBaseOptions)}>
+// //                                 <FontAwesomeIcon className="baseboost-hamburger-bars" icon={faBars} />
+
+// //                             </div>
+
+
+// //                             <div className="baseboost-logo">
+// //                                 <span>|S. N. Valadez|</span>
+// //                             </div>
+
+
+// //                             <div className={showBaseOptions ? "baseboost-options show-hamburger-options" : "baseboost-options"}>
+// {/* //                                 {getBaseBoostOptions()} */}
+// //                             </div>
+
+// //                         </div>
+
+// //            </div>
+
+// //         </div>
+// //     )
+//     }
+
+
+// export default BaseBoost;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,7 +7,7 @@ import "./style.css"
  function Main() {
     return (
         <div className="main-con">
-            <BaseBoost></BaseBoost>
+            {/* //<BaseBoost></BaseBoost> */}
             <Header></Header>
             <Footer></Footer>
             
