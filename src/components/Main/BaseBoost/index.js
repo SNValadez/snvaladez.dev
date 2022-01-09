7069f8e0a32b077ce4c098eb07a95ce500463c4e
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-08 17:02:53 -0800

    update screams in BaseBoost

--- src/components/Main/BaseBoost/index.js
+++ src/components/Main/BaseBoost/index.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {TOTAL_SCREAM, GET_SCREAM_INDIE} from "../../../utilities/comUtils"
+
+import ScreamService from "../../../utilities/ScreamService"
+import "./style.css"
+
+import {faBars} from "@fortawesome/free-solid-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
 function BaseBoost() {
+
+    const [showBaseOptions, setShowBaseOptions] = useState(false);
+    const [selectedScream, setSelectedScream] = useState(0);
+
+const updateCurrentScream = (currentScream)=>{
+
+if(!currentScream || !currentScream.screamInView)
+return;
+let screamIndex = GET_SCREAM_INDIE(currentScream.screamInView)
+if(screamIndex < 0)
+return
+
+}
+
+
+
+let currentScreamSub = ScreamService.currentScreamBroadCaster.subscribe(updateCurrentScream)
+
     return (
         <div>
             
