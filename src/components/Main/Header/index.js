1fb08a39425243aca2c332b0245a0290c18a428f
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-28 22:28:12 -0800

    div space

--- src/components/Main/Header/index.js
+++ src/components/Main/Header/index.js
@@ -96,6 +96,7 @@ function Header() {
                         QuackChat Group Project{" "}
                     </button>
                     </a>
+                    
                 </div>
 
                 </div>
